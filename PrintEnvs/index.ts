5f7d1a01a0857e70d77844fb4d903d66f9604ac5
Francesco Persico <francescopersico@gmail.com>
2020-03-02 08:44:17 +0100

    First commit

---
+++ PrintEnvs/index.ts
@@ -0,0 +1,16 @@
+import { AzureFunction, Context, HttpRequest } from "@azure/functions";
+
+const httpTrigger: AzureFunction = async function(
+  context: Context,
+  req: HttpRequest
+): Promise<void> {
+  context.res = {
+    status: 200,
+    body: JSON.stringify(process.env, undefined, "\t"),
+    headers: {
+      "Content-Type": "application/json"
+    }
+  };
+};
+
+export default httpTrigger;
